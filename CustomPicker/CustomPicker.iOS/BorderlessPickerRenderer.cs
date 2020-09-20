925621f832ed683955703af8589639c971a982bb
victor.okosodo <victor.okosodo@prestacap.com>
2020-09-20 11:07:41 +0200

    first commit

---
+++ CustomPicker/CustomPicker.iOS/BorderlessPickerRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+namespace CustomPicker.iOS
+{
+   public class BorderlessPickerRenderer: PickerRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Picker> e)
+        {
+            base.OnElementChanged(e);
+            var element = (CustomPicker)this.Element;
+            if (this.Control != null && this.Element != null && !string.IsNullOrEmpty(element.Image))
+            {
+                var downarrow = UIImage.FromBundle(element.Image);
+                Control.RightViewMode = UITextFieldViewMode.Always;
+                Control.RightView = new UIImageView(downarrow);
+            }
+        }
+
+    }
+}
---
+++ CustomPicker/CustomPicker/CustomPicker.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CustomPicker
+{
+    public class CustomPicker:Picker
+    {
+		//addded attached property image
+		public static readonly BindableProperty ImageProperty =
+			BindableProperty.Create(nameof(Image), typeof(string), typeof(CustomPicker), string.Empty);
+		public string Image
+		{
+			get { return (string)GetValue(ImageProperty); }
+			set { SetValue(ImageProperty, value); }
+		}
+	}
+}
